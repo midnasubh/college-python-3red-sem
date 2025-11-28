670fd7e08bf5b9ee4a8357173422fe7052ca67cd
midnasubh <midna3171@gmail.com>
2025-11-28 14:47:42 +0530

    Add TF-IDF transformation script for text data representation

---
+++ TF-IDF_transformation_to_text.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+df=pd.DataFrame({
+    "Text":[
+        'Cats are renning quickly',
+
+        "Dogs are barking loudly"
+    ]
+})
+print("Original")
+print(df)
+tf=TfidfVectorizer()
+x=tf.fit_transform(df["Text"])
+tf_df=pd.DataFrame(x.toarray(),columns=tf.get_feature_names_out())
+print("_"*20)
+print("TF-IDF representation ")
+print(tf_df)
+# tf*idf
