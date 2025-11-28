7b2a04310b83accbcaa1b21264dc038b3685c211
midnasubh <midna3171@gmail.com>
2025-11-28 15:13:03 +0530

    hgfdh

---
+++ polynomial_and_interaction.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.preprocessing import PolynomialFeatures
+df=pd.DataFrame({
+    "x1":[2,4,6,3,5],
+    "x2":[3,5,7,8,10]
+})
+print("Oirginal ")
+print(df)
+poly=PolynomialFeatures(degree=2,include_bias=False)
+poly_featues=poly.fit_transform(df)
+feature_name=poly.get_feature_names_out(['x1','x2'])
+poly_df=pd.DataFrame(poly_featues,columns=feature_name)
+print(poly_df)
